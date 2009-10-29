1bce981ca240d423bbb58ebcbbe96cc16a564faf
Van Bui <coscbui@gmail.com>
2009-10-29 02:54:30 +0000

    modify AIX measurement env to correctly run mpi and omp codes

--- AIXMeasurementEnv.py
+++ AIXMeasurementEnv.py
@@ -14,6 +14,8 @@ class AIXMeasurementEnv:
 		print >>f, '#@ job_type = parallel'
 		print >>f, '#@ environment = COPY_ALL'
 		print >>f, '#@ class = medium'
+		if pmodel == 'omp':
+			tasks_per_node = 1
 		print >>f, '#@ tasks_per_node = ' + str(tasks_per_node)
 		print >>f, '#@ node = ' + str(node)
 		print >>f, '#@ network.MPI_LAPI = sn_all,not_shared,US'
@@ -45,55 +47,63 @@ class AIXMeasurementEnv:
 
 	def runApp(self, perfCmd):
 
-    		for p in nodes:
-        		for t in tasks_per_node:
-				if pmodel == 'omp' or pmodel == 'mpi:omp':
-                			os.environ['OMP_NUM_THREADS'] = t
+		if pmodel == 'omp':
+			for p in nodes:
+				for t in threads:
+					os.environ['OMP_NUM_THREADS'] = t
+					self.runit(p,t)
+		if pmodel == 'mpi': 
+   			for p in nodes:
+        			for t in tasks_per_node:
+					self.runit(p,t)
+                			
+
+	def runit(self,p,t):
 				
-                       		cmd = cmdline 
+      		cmd = cmdline 
 
-                		if DEBUG == 1:
-                        		print 'DEBUG: executing: ', cmd
-                       			print 'DEBUG: OMP_NUM_THREADS=', os.environ.get('OMP_NUM_THREADS')
+       		if DEBUG == 1:
+           		print 'DEBUG: executing: ', cmd
+       			print 'DEBUG: OMP_NUM_THREADS=', os.environ.get('OMP_NUM_THREADS')
 
-                        	# Run the application in the specified directory	
-                		if not os.path.exists(workdir): os.makedirs(workdir)
-                		try: os.chdir(workdir)
-                		except Exception,e:
-                        		print >> sys.stderr, 'Exception in ExperimentDriver:  '+str(e)
-                		dest = datadir + '/' + appname + '-' + expname + '-' + trialname + '-p' + p + 't' + t 
-                		if not os.path.exists(dest): os.makedirs(dest)
-                		try: os.chdir(dest)
-                		except Exception,e:
-                        		print >> sys.stderr, 'Exception in ExperimentDriver:  '+str(e)
+               	# Run the application in the specified directory	
+       		if not os.path.exists(workdir): os.makedirs(workdir)
+       		try: os.chdir(workdir)
+       		except Exception,e:
+               		print >> sys.stderr, 'Exception in ExperimentDriver:  '+str(e)
+       		dest = datadir + '/' + appname + '-' + expname + '-' + trialname + '-p' + p + 't' + t 
+       		if not os.path.exists(dest): os.makedirs(dest)
+       		try: os.chdir(dest)
+       		except Exception,e:
+               		print >> sys.stderr, 'Exception in ExperimentDriver:  '+str(e)
 				
-				if exemode == 'interactive':
-	               			app_output = os.popen(cmd)
-					fp = open("expout", "w")
-        	       			print >>fp,  app_output.read()
-					fp.close()
-					if DEBUG == 1:
-						print 'DEBUG: interactive command: ', cmd 
-				elif exemode == 'batch':
-					filename = workdir + '/' + 'mpi-p'+ str(p) + '-t' + str(t) +'.ll'
-					self.genBatchScript(p, t, filename, cmd, dest)
-           				try: os.path.exists(filename)
-		               		except Exception,e:
-                	        		print >> sys.stderr, 'Exception: Batch script does not exist  '+str(e)
-					mcmd = 'mv ' + workdir + '/' + 'mpi-p'+ str(p) + '-t' + str(t) + '.ll ' + dest + '/'
-					commands.getstatusoutput(mcmd)
-					if DEBUG == 1: 
-						print 'DEBUG: move ll files: ', mcmd
+		if exemode == 'interactive':
+       			app_output = os.popen(cmd)
+			fp = open("expout", "w")
+       			print >>fp,  app_output.read()
+			fp.close()
+			if DEBUG == 1:
+				print 'DEBUG: interactive command: ', cmd 
+		elif exemode == 'batch':
+			filename = workdir + '/' + 'mpi-p'+ str(p) + '-t' + str(t) +'.ll'
+			self.genBatchScript(p, t, filename, cmd, dest)
+			try: os.path.exists(filename)
+               		except Exception,e:
+       	        		print >> sys.stderr, 'Exception: Batch script does not exist  '+str(e)
+			mcmd = 'mv ' + workdir + '/' + 'mpi-p'+ str(p) + '-t' + str(t) + '.ll ' + dest + '/'
+			commands.getstatusoutput(mcmd)
+			if DEBUG == 1: 
+				print 'DEBUG: move ll files: ', mcmd
 
-					cmd = batchcmd + ' ' + dest + '/' + 'mpi-p'+ str(p) + '-t' + str(t) + '.ll ' 
-					if DEBUG == 1:
-						print 'DEBUG: batch submit command: ', cmd
-	               			app_output = os.popen(cmd)
-					fp = open("expout", "w")
-        	       			print >>fp,  app_output.read()
-					fp.close()
+			cmd = batchcmd + ' ' + dest + '/' + 'mpi-p'+ str(p) + '-t' + str(t) + '.ll ' 
+			if DEBUG == 1:
+				print 'DEBUG: batch submit command: ', cmd
+       			app_output = os.popen(cmd)
+			fp = open("expout", "w")
+       			print >>fp,  app_output.read()
+			fp.close()
 
-				self.moveData(workdir, dest)
+		self.moveData(workdir, dest)
 
         
 	def loadTrials(self):
