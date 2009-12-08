e4d8095ae2ff40b64e0ae0ae571f19b5fd003d5c
Van Bui <coscbui@gmail.com>
2009-12-08 03:56:45 +0000

    added call to generate merged plots

--- src/examples/drivers/ModelDriver.py
+++ src/examples/drivers/ModelDriver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from me.platforms.xeon import Generic as XeonMeasurementEnv
-from examples.models.logGP_FT import LogGPFT
+from examples.models.logp_FT import FT
 from vis.tools.pylab import Plotter
 
 def main():
@@ -9,9 +9,10 @@ def main():
     
     vm = XeonMeasurementEnv()
     plotter = Plotter()
-    model = LogGPFT()
+    model = FT()
     xdata,ydata = vm.validateModel(model)
     plotter.generatePlot(xdata,ydata)
+    plotter.generateMergedPlot(xdata,ydata)
 
 if __name__ == "__main__":
 
