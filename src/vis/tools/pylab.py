345da0979efb578983b6a193c0fc1e28f262698a
Van Bui <coscbui@gmail.com>
2009-12-08 03:49:51 +0000

    generate merged plots

--- src/vis/tools/pylab.py
+++ src/vis/tools/pylab.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from params import *
-import commands
+import commands,os
 
 from vis.interfaces import AbstractPlotter
 
@@ -34,14 +34,74 @@ class Plotter(AbstractPlotter):
         commands.getstatusoutput(cmd)
         cmd = './model.py'
         commands.getstatusoutput(cmd)
-        cmd = 'chmod u+x plot.py'
+        cmd = 'chmod u+x model-plot.py'
         commands.getstatusoutput(cmd)
+        moveResultsCommand = 'mv ' + os.getcwd() + '/model.py ' + resultsdir
+        moveResultsCommand2 = 'mv ' + os.getcwd() + '/model-plot.py ' + resultsdir
+        commands.getstatusoutput(moveResultsCommand)
+        commands.getstatusoutput(moveResultsCommand2)
+
+    def generateMergedPlot(self, xdata, ydata):
+        
+        f = open('merged-plot.py','w')
+        print >>f, '#!/usr/bin/env python'
+        print >>f, 'from pylab import *'
+        print >>f, 'import matplotlib.pyplot as plt'
+
+        data1 = 't = ['
+
+        for x in xdata:
+            data1 += str(x) + ','
+
+        data1 += ']'
+        data2 = 'r = ['
+
+        for y in ydata:
+            data2 += str(y) + ','
+
+        data2 += ']'
+
+        print >>f, data1
+        print >>f, data2
+        
+        filename = resultsdir + '/plot.py' 
+        infile = open(filename,"r")
+        text = infile.read()
+        infile.close()
+        search = "s = "
+        index = text.find(search)
+        index2 = text.find("\n", index)
+        data3 = text[index:index2]
+
+        print >>f, data3
+        print >>f, 'fig = plt.figure()'
+        print >>f, 'ax = fig.add_subplot(111)'
+        print >>f, 'p1, = ax.plot(t, s, \'k--\')'
+        print >>f, 'p2, =  ax.plot(t, r,\'k:\')'
+
+        print >>f, 'ax.set_label("', xaxislabel, '")'
+
+        print >>f, 'ax.set_ylabel("', yaxislabel, '")'
+
+        print >>f, 'ax.set_title("', graphtitle,'")'
+
+        print >>f, 'ax.legend((p1,p2),("', legend[0],'", "', legend[1],'"))'
+
+        print >>f, 'ax.grid(True)'
+        print >>f, 'plt.show()'
+
+        f.close()
+
+        cmd = 'chmod u+x merged-plot.py'
+        commands.getstatusoutput(cmd)
+        moveResultsCommand = 'mv ' + os.getcwd() + '/merged-plot.py ' + resultsdir
+        commands.getstatusoutput(moveResultsCommand)
 
     def writeGeneratePlot(self, f):
 
         print >>f, 'def generatePlot(xdata,ydata):'
 
-        print >>f, '\tf=open(\'plot.py\',\'a\')'
+        print >>f, '\tf=open(\'model-plot.py\',\'a\')'
 
         print >>f, '\tprint >>f, \'#!/usr/bin/env python\''
         print >>f, '\tprint >>f, \'\\nfrom pylab import *\''
