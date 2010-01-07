e1dcadc1911c134a718412f340dcb79a1501008c
Van Bui <coscbui@gmail.com>
2010-01-07 03:53:39 +0000

    added support for nodes and tasks per node

--- src/analysis/tools/gprof.py
+++ src/analysis/tools/gprof.py
@@ -12,16 +12,30 @@ class Gprof(AbstractAnalyzer):
 
         ydata = []
 
-        for p,o in map(None, processes, cmdlineopts):
-            for t in threads:
-                filename = datadir + '/' + appname + '-' + expname + '-' + trialname + '-' + 'p' + p +'t' + t + '/' + 'gprof.out'
-                f = file(filename)
-                for line in f:
-                    if programevent in line:
-                        break
-                words = line.split()
-                print words[1]
-                ydata.append(words[1])
+	if nodes and tasks_per_node:
+	    for p,o in map(None, nodes, cmdlineopts):
+                for t in tasks_per_node:
+                    filename = datadir + '/' + appname + '-' + expname + '-' + trialname + '-' + 'p' + p +'t' + t + '/' + 'gprof.out'
+                    f = file(filename)
+                    for line in f:
+                        if programevent in line:
+                            break
+                    words = line.split()
+   	            i = words.index(programevent)
+		    avgtime = float(words[i+1]) / int(p)	
+                    ydata.append(str(avgtime))
+
+	else:
+            for p,o in map(None, processes, cmdlineopts):
+                for t in threads:
+                    filename = datadir + '/' + appname + '-' + expname + '-' + trialname + '-' + 'p' + p +'t' + t + '/' + 'gprof.out'
+                    f = file(filename)
+                    for line in f:
+                        if programevent in line:
+                            break
+                    words = line.split()
+                    print words[1]
+                    ydata.append(words[1])
 
         self.writeGeneratePlot(ydata)
                  
@@ -31,9 +45,15 @@ class Gprof(AbstractAnalyzer):
         plotter = Plotter()
 
         if pmodel == 'mpi':
-            for n in processes:
-                P = n
-                xdata.append(P)
+	    if nodes:
+                for n in nodes:
+                    P = n
+                    xdata.append(P)
+
+            else:	
+                for n in processes:
+                    P = n
+                    xdata.append(P)
 
         plotter.genPlot(xdata, ydata)
 
