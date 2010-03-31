899050bfde436b972729844ec01ab6fe8337a1a5
Van Bui <coscbui@gmail.com>
2010-03-31 21:54:11 +0000

    added no timer option for me

---
+++ src/me/tools/notimer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from params import *
+import os
+from me.interfaces import AbstractCollector
+
+class Collector(AbstractCollector):
+
+    def setCounters(self):
+       pass
+			
+    def getCommand(self):
+
+        mycommand = ''
+        
+        return mycommand
+
+    def getDataFormat(self):
+        
+        return 'notimer'
