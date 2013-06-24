fe9f7b77695ce6c08d6bdab0aaa61a4acc8c2e79
Kathleen Chalas <kchalas@knox.edu>
2013-06-24 20:45:27 +0000

    l1_read_bw try 2, not tested

--- src/bench/platforms/tests.py
+++ src/bench/platforms/tests.py
@@ -13,9 +13,9 @@ def main():
 
     # test various metrics computations
 
-    #arch.measure('mem_read_bw', procs='1', size='256m', reps='5')
+    arch.measure('mem_read_bw', procs='1', size='256m', reps='5')
     #arch.measure('mem_read_bw', procs='2', size='256m', reps='1')
-    arch.measure('l1_read_latency', size='32', stride='128')
+    #arch.measure('l1_read_latency', size='32', stride='128')
     #arch.measure('l1_read_bw', procs='1', size='32m', reps='5')
 
 
--- src/bench/platforms/x86.py
+++ src/bench/platforms/x86.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, sys, getopt, re
+import os, sys, getopt, re, math
 
 from bench.interfaces import AbstractPlatform, Measurement
 from common.messages import err,warn,info,debug,exit
@@ -70,20 +70,57 @@ class X86(AbstractPlatform):
 
     def get_l1_read_bw(self, **kwargs):
         ''' Measure L1 read bandwidth and record in self.measurements. '''
-        size = kwargs.get('size')
         procs = kwargs.get('procs')
         reps = kwargs.get('reps')
-        cmd = self.lmbench_path + 'bw_mem -P %s %s rd' % (procs,size)
+      
+        #depending on size (which one you're looking for) can start around that range
+        start = int(kwargs.get('size'))
+        end = int(kwargs.get('next_size'))
 
-        vals = []
-        self.log(cmd)
-        for i in range(0,int(reps)):
-            return_code, cmd_output = system_or_die(cmd, log_file=self.logfile)
-            s,val = cmd_output.split()
-            vals.append(float(val))
-             
-        params = {'metric':'l1_read_bw','size':s,'procs':procs,'reps':reps}
-        self.recordMeasurement(params, Measurement(get_stats(vals),units='MB/s',params=params))
+        means = []
+        stat_collector = { }
+
+        for x in range(start, end):
+            vals = []
+            cmd = self.lmbench_path + 'bw_mem -P %s %s rd' %  (procs, x)
+            self.log(cmd)
+            #get read bandwidth for a specific size
+            for i in range(0,int(reps)):
+                """number of repetitions added as a parameter"""
+                return_code, cmd_output = system_or_die(cmd, log_file=self.logfile)
+                s,val = cmd_output.split()
+                vals.append(float(val))
+
+            #keep statistics to pass
+            stat_collector[x] = get_stats(vals)
+            value_to_jump = stat_collector[x[0]]
+            means.append(value_to_jump)
+            
+        jumps = { }
+        #get differences between values to find biggest jump
+        for i in range(0, len(means)):
+            if(i+1 < len(means)):
+                jumps[means[i]] = float(abs(means[i+1] - means[i]))
+                    
+        #find the maximum jump
+        maximum = 0
+        track = -1
+        for i in jumps:
+            if(jumps[i] > maximum):
+                maximum = jumps[i] 
+                track = i
+       
+        mem_size = 0
+        best = []
+        #find final answers by looking through stat_collector
+        for k, v in stat_collector.iteritems():
+            if (v[0] == track):
+                best = v
+                mem_size = k
+
+        #process vals 
+        params = {'metric':'l1_read_bw','size':mem_size,'procs':procs,'reps':reps}
+        self.recordMeasurement(params, Measurement(best,units='MB/s',params=params))
 
         return
 
