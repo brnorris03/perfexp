392883c5a69a202d3abae1586e53ad8246bd3dbd
Van Bui <coscbui@gmail.com>
2013-06-25 21:07:44 +0000

    globals now can read a config file

--- src/common/globals.py
+++ src/common/globals.py
@@ -4,6 +4,7 @@
 # For copying information, see the file LICENSE
 
 import logging, os
+import ConfigParser
 
 # Global singleton class for various runtime options 
 def Globals():
@@ -50,4 +51,64 @@ class Globals_Singleton:
         else: 
             self.debug = False
             self.logger.setLevel(logging.INFO)
-        pass
+#        pass
+
+        # initialize configuration file parameters
+        self.configparams = {'lmbenchdir':None, 'blackjackdir':None, 'skampidir':None, 'papidir':None,'taudir':None,'hpctoolkitdir':None,'perfsuitedir':None}
+        self.config = ConfigParser.ConfigParser()
+        self.config_file = os.environ.get("PERFEXPDIR") + '/src/config/config.txt'
+        
+    def _processConfigFile(self):
+        
+        benchmarksection = 'Benchmarks'
+        toolssection = 'Tools'
+
+        self.config.readfp(open(self.config_file))
+
+        try:
+            newparam = 'lmbenchdir'
+            self.configparams[newparam] = self.config.get(benchmarksection, newparam)
+        except:
+            raise Exception('Error: could not find lmbenchdir in configuration file %s' % self.config_file)
+
+
+        try:
+            newparam = 'blackjackdir'
+            self.configparams[newparam] = self.config.get(benchmarksection, newparam)
+        except:
+            raise Exception('Error: could not find blackjackdir in configuration file %s' % self.config_file)
+
+        try:
+            newparam = 'skampidir'
+            self.configparams[newparam] = self.config.get(benchmarksection, newparam)
+        except:
+            raise Exception('Error: could not find skampidir in configuration file %s' % self.config_file)
+
+
+        try:
+            newparam = 'papidir'
+            self.configparams[newparam] = self.config.get(toolssection, newparam)
+        except:
+            raise Exception('Error: could not find papidir in configuration file %s' % self.config_file)
+
+        try:
+            newparam = 'taudir'
+            self.configparams[newparam] = self.config.get(toolssection, newparam)
+        except:
+            raise Exception('Error: could not find taudir in configuration file %s' % self.config_file)
+
+        try:
+            newparam = 'hpctoolkitdir'
+            self.configparams[newparam] = self.config.get(toolssection, newparam)
+        except:
+            raise Exception('Error: could not find hpctoolkitdir in configuration file %s' % self.config_file)
+
+        try:
+            newparam = 'perfsuitedir'
+            self.configparams[newparam] = self.config.get(toolssection, newparam)
+        except:
+            raise Exception('Error: could not find perfsuitedir in configuration file %s' % self.config_file)
+
+
+
+
