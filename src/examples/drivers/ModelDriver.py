e70cf27d3022ede7cbdc002cd2169840113483e6
Brandon Hildreth <bthildreth@gmail.com>
2014-02-03 18:38:55 -0800

    ModelDriver comment
    Added comments to ModelDriver.py

--- src/examples/drivers/ModelDriver.py
+++ src/examples/drivers/ModelDriver.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+# -------
+# imports
+# -------
+
 from me.platforms.xeon import Generic as XeonMeasurementEnv
 from me.platforms.aix import BluePrint as AIXMeasurementEnv
 from examples.models.logp_FT import FT
@@ -11,32 +15,34 @@ from me.params import MEParams
 from examples.models.params import GENParams
 import os, commands
 
-def main():
 
+# ----
+# main
+# ----
+def main():
+    # Get params and examples directories
     paramsdir = os.environ.get("PERFEXPDIR") + '/src/examples/params'
     examplesdir = os.environ.get("PERFEXPDIR") + '/src/examples'
-
+    
+    # Save contents of params directory
     dirList=os.listdir(paramsdir)
-
+    # Iterate through all 'params' files
     for fname in dirList:
         if not fname.startswith('.'):
+            # Copy params.txt file to src/examples/
             filename = paramsdir + '/' + fname
             cpcmd = 'cp ' + filename + ' ' + examplesdir + '/params.txt'
             commands.getstatusoutput(cpcmd)
-
-
+            # Get values from params file
             modParams = RARMAParams()
             modParams._processConfigFile()
-
             ansParams = ANSParams()
             ansParams._processConfigFile()
-
             meParams = MEParams()
             meParams._processConfigFile()
-            
             genParams = GENParams()
             genParams._processConfigFile()
-    
+            # comment needed
             vm = AIXMeasurementEnv()
             plotter = Plotter()
             model = RARMA()
@@ -45,5 +51,4 @@ def main():
 #           plotter.generateMergedPlot(xdata,ydata)
 
 if __name__ == "__main__":
-
     main()
