4850f2f5825996aeaf1e91395074904b5ef7a332
Brandon Hildreth <bthildreth@gmail.com>
2014-02-03 18:30:27 -0800

    AnalysisDriver comments
    Added comments to AnalysisDriver.py

--- src/examples/drivers/AnalysisDriver.py
+++ src/examples/drivers/AnalysisDriver.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+# -------
+# imports
+# -------
 
 from analysis.tools.hpctoolkit import HPCToolkit
 from analysis.metrics.time import WallClock
@@ -7,25 +10,29 @@ from analysis.params import ANSParams
 from storage.params import DBParams 
 from me.params import MEParams
 
-def main():
 
+# ----
+# main
+# ----
+
+def main():
+    # Get 'ExperimentDriver' values from params file
     meParams = MEParams()
     meParams._processConfigFile()
-
+    # Get 'DataManager' values from params file
     dbParams = DBParams()
     dbParams._processConfigFile()
-
+    # Get 'Analysis' values from params file
     ansParams = ANSParams()
     ansParams._processConfigFile()
- 
+    # needs comment
     try: 
         metric = WallClock(params=ANSParams.ansparams['metricparams'])
     except: 
         metric = WallClock(params={})
-        
+    # Create analysis tool and launch  
     analysis = HPCToolkit()
     analysis.runAnalysis(metric)
 
 if __name__ == "__main__":
-
     main()
